4eb6076b7d2516c073e7d3371c485f885b4c74a4
Yifeng Huang <me@nongraphical.com>
2011-03-02 11:28:25 -0800

    Added basic command framework to FinalProject

--- FinalProject/Main.cs
+++ FinalProject/Main.cs
@@ -3,8 +3,17 @@ namespace FinalProject
 {
 	public class Recognizer
 	{
+		enum Command { Train, TestRecognize, Help };
 		static public void Main(string[] args)
 		{
+			Command c = Recognizer.Command.TestRecognize;
+			if ( args.Length > 0 ) {
+				c = (Command)Enum.Parse(typeof(Recognizer.Command), args[0]);
+				if ( c == Recognizer.Command.Help ) {
+					foreach ( var cm in Enum.GetNames(typeof(Recognizer.Command)) ) Console.WriteLine(cm);
+					return;
+				}
+			}
 		}
 	}
 }
